0f4e19ad79507c18ff45cecb8b9e1618914820b0
petrchili <138665285+petrchili@users.noreply.github.com>
2023-07-07 10:34:22 +0300

    Add files via upload

---
+++ FlapCatCopters/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async Events_game_Event1_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#fbfbd4'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
