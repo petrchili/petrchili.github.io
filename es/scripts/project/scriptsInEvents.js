1f03c4cd9cdc79d3a89b1188c11fac55dbe841c5
petrchili <138665285+petrchili@users.noreply.github.com>
2023-08-22 02:29:04 +0300

    Add files via upload

---
+++ es/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async EventMenu_Event5_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#524f77'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
